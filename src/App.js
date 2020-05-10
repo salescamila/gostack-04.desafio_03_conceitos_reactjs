d4269efe201edb6150de261f2674cfdfc970b9cf
Camila Sales <salescamila1@gmail.com>
2020-05-09 22:21:07 -0400

    feat: Listar repositórios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, [repositories]);
+
   async function handleAddRepository() {
-    // TODO
+    // TO-DO
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+    // TO-DO
+  };
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo => {
+           return(
+            <li key={repo.id}>{repo.title}
+              <button onClick={() => handleRemoveRepository(1)}>
+                Remover
+              </button>
+            </li>
+           )}
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
-}
+};
 
 export default App;
